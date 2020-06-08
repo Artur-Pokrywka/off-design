d22411736eecc0127d84db3a90b137e30d21334d
Artur <pokrywkaart@gmail.com>
2020-06-08 16:31:32 +0200

    fix gh pages config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
     pathPrefix: "/off-design",
     siteMetadata: {
-        title: `off design-wnętrza`,
-        description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-        author: `Artur Pokrywka`,
+        title: "off design-wnętrza",
+        description: "Kick off your next, great Gatsby project with this default starter.",
+        author: "Artur Pokrywka",
     },
     plugins: [
         `gatsby-plugin-react-helmet`,
