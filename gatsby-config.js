f5972dc09e18562099d61012a7a234ced1d03f2a
Artur <pokrywkaart@gmail.com>
2020-06-08 13:29:52 +0200

    Set gh pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,46 +1,45 @@
 module.exports = {
-  siteMetadata: {
-    title: `off design-wnętrza`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `Artur Pokrywka`,
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-styled-components`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+    pathPrefix: "/off-design",
+    siteMetadata: {
+        title: `off design-wnętrza`,
+        description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+        author: `Artur Pokrywka`,
     },
-    {
-      resolve: `gatsby-plugin-prefetch-google-fonts`,
-      options: {
-        fonts: [
-          {
-            family: `Montserrat`,
-            subsets: [`latin-ext`],
-            variants: [`200`, `700`]
-          },
-        ],
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `black`,
-        theme_color: `black`,
-        display: `minimal-ui`,
-        icon: `src/images/icons/sell.png`
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-  ],
+    plugins: [
+        `gatsby-plugin-react-helmet`,
+        `gatsby-plugin-styled-components`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/images`,
+            },
+        },
+        {
+            resolve: `gatsby-plugin-prefetch-google-fonts`,
+            options: {
+                fonts: [{
+                    family: `Montserrat`,
+                    subsets: [`latin-ext`],
+                    variants: [`200`, `700`]
+                }, ],
+            },
+        },
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `gatsby-starter-default`,
+                short_name: `starter`,
+                start_url: `/`,
+                background_color: `black`,
+                theme_color: `black`,
+                display: `minimal-ui`,
+                icon: `src/images/icons/sell.png`
+            },
+        },
+        // this (optional) plugin enables Progressive Web App + Offline functionality
+        // To learn more, visit: https://gatsby.dev/offline
+    ],
 }
